91a1082d0646ada98162a5ff84d80f6056b830e5
Raiyan <ray99scholars@gmail.com>
2025-06-22 21:35:06 +0600

    Added popular menu section in home page

--- src/Pages/Home/Menu.jsx
+++ src/Pages/Home/Menu.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SectionTitle from '../Shared/SectionTitle';
+import MenuItem from '../Shared/MenuItem';
 
 const Menu = () => {
+
+    const [menudata, setMenudata] = useState([]);
+
+    useEffect(() => {
+        fetch('menu.json')
+        .then(res => res.json())
+        .then(data => {
+            const popular = data.filter(d => d.category === 'popular')
+            setMenudata(popular)
+        });
+    }, [])
+
+    console.log(menudata);
+    
+
+
     return (
-        <div className='my-24'>
+        <div className='my-24 w-2/3 mx-auto'>
 
             <SectionTitle heading={'from our menu'} subheading={'Check it out'}></SectionTitle>
+
+            <div className='grid grid-cols-2 gap-8'>
+                {
+                    menudata.map(data => <MenuItem key={data._id} item={data}></MenuItem> )
+                }
+            </div>
+
+            <div className='flex justify-center items-center'>
+                <button className='text-black text-center mx-auto uppercase mt-12 border-b-2 border-black rounded-md pb-2 px-4'>View Full Menu</button>
+            </div>
+
             
         </div>
     );
---
+++ src/Pages/Shared/MenuItem.jsx
@@ -0,0 +1,22 @@
+
+
+const MenuItem = ({item}) => {
+    const {name, image, recipe, price} = item;
+
+    return (
+        <div className="text-black flex items-center gap-4">
+
+            <img src={image} alt="menuPic" className="w-28 h-24 rounded-tl-none rounded-full" />
+
+            <div>
+                <h3 className="uppercase text-lg mb-4">{name}------------------</h3>
+                <p  className="text-gray-400">{recipe}</p>
+            </div>
+
+            <p className="self-start text-yellow-500">${price}</p>
+            
+        </div>
+    );
+};
+
+export default MenuItem;
