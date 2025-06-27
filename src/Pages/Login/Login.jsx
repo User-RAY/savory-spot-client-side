dfed65bb06499d32144031030047be38e98356d9
Raiyan <ray99scholars@gmail.com>
2025-06-27 21:52:08 +0600

    Added login page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,20 +2,17 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div className='min-h-[80vh] bg-[url("/authentication.png")]'>
 
-            <div className='w-[60%] h-full mx-auto'>
-
-                <div>
-                    <img src="/authentication2.png" alt="image" className='w-1/2' />
+        <div className='py-60 bg-[url("/authentication.png")]'>
+            <div className='w-[80%] mx-auto flex justify-center items-center'>
+                <div className='w-[50%]'>
+                    <img src="/authentication2.png" alt="image" className='' />
                 </div>
-
-                <div>
-                    Login Form
+                <div className='text-black w-[50%] text-center'>
+                    <h1 className='text-4xl'>Login Form</h1>
                 </div>
-
             </div>
-            
+        
         </div>
     );
 };
