7c22df720a980d7d2dfa854ff06f0ebf72d31e65
Raiyan <ray99scholars@gmail.com>
2025-08-12 23:36:24 +0600

    Added back to home button on error page

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -1,5 +1,11 @@
+import { NavLink } from "react-router-dom";
+import { FaHome } from "react-icons/fa";
+
 
 const Error = () => {
+
+    const activeLink = ({ isActive }) => isActive  ? "text-yellow-400" : "";
+
     return (
         <div className="bg-white h-screen">
             
@@ -7,6 +13,8 @@ const Error = () => {
                 
                 <img src="../../../src/assets/404.gif" className="object-contain w-96" alt="" />
                 <p className="text-red-800 text-2xl">Error</p>
+
+                <button><NavLink to='/' className={`${activeLink} btn bg-[#B58130] text-white mt-10`}><FaHome /> Back to HOME</NavLink></button> 
                 
             </div>
 
