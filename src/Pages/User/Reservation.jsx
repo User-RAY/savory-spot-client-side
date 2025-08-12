795a961898053ecfae7805d9ab44ab906db10438
Raiyan <ray99scholars@gmail.com>
2025-08-13 01:19:41 +0600

    Added Our Location section in user reservation page

--- src/Pages/User/Reservation.jsx
+++ src/Pages/User/Reservation.jsx
@@ -23,7 +23,7 @@ const Reservation = () => {
             <div className="w-[80%] mx-auto mt-12">
                 <SectionTitle heading={'book a table'} subheading={'reservation'}></SectionTitle>
 
-                <div>
+                <div className='mb-12'>
 
                     <form onSubmit={handleSubmit(handleReservation)} className='grid grid-cols-3 gap-12'>
 
@@ -93,6 +93,36 @@ const Reservation = () => {
 
                 </div>
 
+                <SectionTitle heading={'our loaction'} subheading={'Visit Us'}></SectionTitle>
+
+
+                <div className='grid grid-cols-3 gap-4 text-black mx-auto mb-16'>
+
+                    <div className='border border-gray-300'>
+                        <h1 className='bg-[#D1A054] text-center py-4'>Phone</h1>
+                        <div className='bg-gray-200 w-[80%] mx-auto text-center px-12 pb-12 pt-8'>
+                            <h1 className='uppercase mb-2'>phone</h1>
+                            <h3>+38 (012) 34 56 789</h3>
+                        </div>
+                    </div>
+
+                    <div className='border border-gray-300'>
+                        <h1 className='bg-[#D1A054] text-center py-4'>Address</h1>
+                        <div className='bg-gray-200 w-[80%] mx-auto text-center px-12 pb-12 pt-8'>
+                            <h1 className='uppercase mb-2'>Address</h1>
+                            <h3>+38 (012) 34 56 789</h3>
+                        </div>
+                    </div>
+
+                    <div className='border border-gray-300'>
+                        <h1 className='bg-[#D1A054] text-center py-4'>Working hours</h1>
+                        <div className='bg-gray-200 w-[80%] mx-auto text-center px-12 pb-12 pt-8'>
+                            <h1 className='uppercase mb-2'>Working hours</h1>
+                            <h3>+38 (012) 34 56 789</h3>
+                        </div>
+                    </div>
+
+                </div>
 
             </div>
 
