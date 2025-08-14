56e96a90b640539f05e3600933f3bcd9263e8745
Raiyan <ray99scholars@gmail.com>
2025-08-14 20:03:37 +0600

    Added star rating and made changes to input in form

--- src/Pages/User/AddReview.jsx
+++ src/Pages/User/AddReview.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
 import SectionTitle from '../Shared/SectionTitle';
 import { useForm } from 'react-hook-form';
 import { IoRocket } from "react-icons/io5";
+import { Rating } from 'react-simple-star-rating'
 
-
+import './review.css'
+import { useState } from 'react';
 
 
 const AddReview = () => {
 
+    const [rating, setRating] = useState(0);
 
     const { register, handleSubmit } = useForm();
 
+    const handleRating = (rate) => {
+        console.log(rate);
+        setRating(rate);
+    }
+
     const handleReview = (data) => {
-        console.log(data);
-        
+        console.log(data, rating);
+
     }
 
 
@@ -28,15 +35,19 @@ const AddReview = () => {
                 <div className='p-16 bg-gray-200 mb-12'>
                     <h1 className='text-center text-3xl'>RATE US</h1>
 
-                    <p className='text-center my-62'>star</p>
 
                     <form onSubmit={handleSubmit(handleReview)} >
+
+                        <div className='flex justify-center mt-6'>
+                             <Rating  onClick={handleRating}/>
+                        </div>
+
 
                         <div className="form-control">
                             <label className="label flex my-6" htmlFor='like'>
                                 <span className="label-tex font-bold text-black">Which recipe you liked most?</span>
                             </label>
-                            <textarea name='like' id='like' placeholder='Recipe you liked most' className="  w-full" {...register("like", {required: true})} />
+                            <input name='like' id='like' placeholder='Recipe you liked most' className="input input-bordered py-8 w-full" {...register("like", {required: true})} />
                         </div>
 
 
@@ -44,7 +55,7 @@ const AddReview = () => {
                             <label className="label flex my-6" htmlFor='suggestion'>
                                 <span className="label-tex font-bold text-black">Do you have any suggestion for us?</span>
                             </label>
-                            <textarea  name='suggestion' placeholder='Suggestion'  id='suggestion' className="  w-full" {...register("suggestion", {required: true})} />
+                            <input  name='suggestion' placeholder='Suggestion'  id='suggestion' className="input input-bordered py-8 w-full" {...register("suggestion", {required: true})} />
                         </div>
 
 
@@ -52,7 +63,7 @@ const AddReview = () => {
                             <label className="label flex my-6" htmlFor='express'>
                                 <span className="label-tex font-bold text-black">Kindly express your care in a short way.</span>
                             </label>
-                            <textarea rows={5} name='express' placeholder='Review in detail'  id='express' className="  w-full" {...register("express", {required: true})} />
+                            <textarea rows={5} name='express' placeholder='Review in detail'  id='express' className="p-4 w-full" {...register("express", {required: true})} />
                         </div>
 
                         
